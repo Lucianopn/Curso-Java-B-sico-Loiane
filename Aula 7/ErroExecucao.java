45842cfb5567a32cd32baaddb1282da514bd82e9
Lucianopn <luciano.pn@gmail.com>
2017-01-16 00:14:22 -0200

    Add files via upload

---
+++ Aula 7/ErroExecucao.java
@@ -0,0 +1,9 @@
+class ErroExecucao
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Você digitou: " + args[0]);
+		//erro divisão 0
+		System.out.println("Você digitou: " + 1/0);
+	}
+}
---
+++ Aula 7/ErroSemantica.java
@@ -0,0 +1,8 @@
+class HelloWorld
+{
+	public static void main(String[] args)
+	{
+		int cont;
+		System.out.println("Você digitou: " + cont);
+	}
+}
