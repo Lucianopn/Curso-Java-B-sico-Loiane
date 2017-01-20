c0849a6482756c1e6117f30d51c70c6626984877
Lucianopn <luciano.pn@gmail.com>
2017-01-19 23:31:40 -0200

    Add files via upload

---
+++ aula15/labs/Exerc07.java
@@ -0,0 +1,35 @@
+package luciano.cursojava.aula15.labs;
+
+import java.util.Scanner;
+
+public class Exerc07 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Digite um número: ");
+		double n1 = scan.nextDouble();
+		
+		System.out.println("Digite um número: ");
+		double n2 = scan.nextDouble();
+		
+		System.out.println("Digite um número: ");
+		double n3 = scan.nextDouble();
+		
+		if (n1>=n2&n1>=n3) {
+			System.out.println("O número " +n1 +" é o maior número.");
+		}else if (n2>=n1&n1>=n3) {
+			System.out.println("O número " +n2 +" é o maior número.");
+		}else
+			System.out.println("O número " +n3 +" é o maior número.");
+		if (n1<=n2&n1<=n3) {
+			System.out.println("O número " +n1 +" é o meno número.");
+		}else if (n2<=n1&n1<=n3) {
+			System.out.println("O número " +n2 +" é o menor número.");
+		}else
+			System.out.println("O número " +n3 +" é o menor número.");
+		
+
+	}
+
+}
