8e09d71be5390e924e9d2cf694493fa4621cc23a
Lucianopn <luciano.pn@gmail.com>
2017-01-20 14:09:18 -0200

    Update Exerc08.java

--- aula15/labs/Exerc08.java
+++ aula15/labs/Exerc08.java
@@ -2,26 +2,26 @@ package luciano.cursojava.aula15.labs;
 
 import java.util.Scanner;
 
-public class Exerc08 {
+public class Exer08 {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		
-		System.out.println("Digite o preço do produto1: ");
+		System.out.println("Digite o preÃ§o do produto1: ");
 		double p1 = scan.nextDouble();
 		
-		System.out.println("Digite o preço do produto2: ");
+		System.out.println("Digite o preÃ§o do produto2: ");
 		double p2 = scan.nextDouble();
 		
-		System.out.println("Digite o preço do produto3: ");
+		System.out.println("Digite o preÃ§o do produto3: ");
 		double p3 = scan.nextDouble();
 		
 		if (p1<=p2&p1<=p3) {
-			System.out.println("O produto " +p1 +" é o mais barato.");
+			System.out.println("O produto " +p1 +" Ã© o mais barato.");
 		}else if (p2<=p1&p1<=p3) {
-			System.out.println("O produto " +p2 +" é o mais barato.");
+			System.out.println("O produto " +p2 +" Ã© o mais barato.");
 		}else
-			System.out.println("O produto " +p3 +" é o mais barato.");
+			System.out.println("O produto " +p3 +" Ã© o mais barato.");
 
 	}
 
