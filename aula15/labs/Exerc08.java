cf723d023e631dfc91b372c70535112bb482aada
Lucianopn <luciano.pn@gmail.com>
2017-01-19 23:38:10 -0200

    Add files via upload

---
+++ aula15/labs/Exerc08.java
@@ -0,0 +1,28 @@
+package luciano.cursojava.aula15.labs;
+
+import java.util.Scanner;
+
+public class Exerc08 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Digite o preço do produto1: ");
+		double p1 = scan.nextDouble();
+		
+		System.out.println("Digite o preço do produto2: ");
+		double p2 = scan.nextDouble();
+		
+		System.out.println("Digite o preço do produto3: ");
+		double p3 = scan.nextDouble();
+		
+		if (p1<=p2&p1<=p3) {
+			System.out.println("O produto " +p1 +" é o mais barato.");
+		}else if (p2<=p1&p1<=p3) {
+			System.out.println("O produto " +p2 +" é o mais barato.");
+		}else
+			System.out.println("O produto " +p3 +" é o mais barato.");
+
+	}
+
+}
---
+++ aula15/labs/Exerc09.java
@@ -0,0 +1,23 @@
+package luciano.cursojava.aula15.labs;
+
+import java.util.Scanner;
+
+public class Exerc09 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Digite um número: ");
+		double n1 = scan.nextDouble();
+		
+		System.out.println("Digite um número: ");
+		double n2 = scan.nextDouble();
+		
+		System.out.println("Digite um número: ");
+		double n3 = scan.nextDouble();
+		
+		
+
+	}
+
+}
