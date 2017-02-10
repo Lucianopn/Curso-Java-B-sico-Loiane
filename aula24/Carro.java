1f6b2eb53da85536bfaf70b0ef1919f330ec4804
Lucianopn <luciano.pn@gmail.com>
2017-02-10 19:33:38 -0200

    Add files via upload

---
+++ aula24/Carro.java
@@ -0,0 +1,11 @@
+package luciano.cursojava.aula24;
+
+public class Carro {
+
+	String marca;
+	String modelo;
+	int numPassageiros; 
+	double capCombustivel; // capacidade combustível
+	double consumoCombustivel;
+	
+}
